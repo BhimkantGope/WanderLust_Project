eb1b2f1a1049f4bb6b739ccedbd10f1d5ed7f2e4
BHIMKANT GOPE <147872144+BhimkantGope@users.noreply.github.com>
2025-07-20 11:59:30 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../../middleware.js");
+const { saveRedirectUrl } = require("../middleware.js");
 
-const userController = require("../../controllers/users.js");
+const userController = require("../controllers/users.js");
 
 router
     .route("/signup")
